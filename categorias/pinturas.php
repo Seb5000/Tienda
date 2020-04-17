19dc499fc61f80cc0bc54d268e89d654a1b78483
Seb500 <sebastian.villarpando@gmail.com>
2020-04-17 13:34:46 -0400

    cambios 17/4/2020

--- categorias/pinturas.php
+++ categorias/pinturas.php
@@ -1,257 +1,39 @@
 <!DOCTYPE html>
-<html lang="">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/productos2.css">
-    <link href="https://fonts.googleapis.com/css?family=Baloo+Thambi+2|Roboto+Condensed&display=swap" rel="stylesheet">
-    <title></title>
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/menu_categorias.css">
 </head>
 
 <body>
-    <div class="barTop">
-        <ul class="contenedor_menu_top" id="menu">
-            <li class="imgContainer">
-                <img src="imagenes/iconos/artist.png" class="icono" alt="">
-                <div class="descripcion">Pinturas</div>
-                <div class="menu menu_pinturas">
-                    <div class="fondo">PINTURAS</div>
-                    <div class="contenedor">
-                        <div class="titulo">
-                            PINTURAS
-                        </div>
-                        <div class="gridDescripcion">
-                            <div class="item-grid">elemento 1</div>
-                            <div class="item-grid">elemento 2</div>
-                            <div class="item-grid">elemento 3</div>
-                            <div class="item-grid">elemento 4</div>
-                            <div class="item-grid">elemento 5</div>
-                            <div class="item-grid">elemento 6</div>
-                            <div class="item-grid">elemento 7</div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="imgContainer">
-                <img src="imagenes/iconos/cut.png" class="icono" alt="">
-                <div class="descripcion">Tijeras</div>
-                <div class="menu">
-                    <div class="fondo">TIJERAS</div>
-                    <div class="contenedor">
-                        <div class="titulo">
-                            TIJERAS
-                        </div>
-                        <div class="gridDescripcion">
-                            <div class="item-grid">elemento 1</div>
-                            <div class="item-grid">elemento 2</div>
-                            <div class="item-grid">elemento 3</div>
-                            <div class="item-grid">elemento 4</div>
-                            <div class="item-grid">elemento 5</div>
-                            <div class="item-grid">elemento 6</div>
-                            <div class="item-grid">elemento 7</div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="imgContainer">
-                <img src="imagenes/iconos/graffiti.png" class="icono" alt="">
-                <div class="descripcion">Papeles</div>
-                <div class="menu">
-                    <div class="fondo">PAPELES</div>
-                    <div class="contenedor">
-                        <div class="titulo">
-                            PAPELES
-                        </div>
-                        <div class="gridDescripcion">
-                            <div class="item-grid">elemento 1</div>
-                            <div class="item-grid">elemento 2</div>
-                            <div class="item-grid">elemento 3</div>
-                            <div class="item-grid">elemento 4</div>
-                            <div class="item-grid">elemento 5</div>
-                            <div class="item-grid">elemento 6</div>
-                            <div class="item-grid">elemento 7</div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="imgContainer">
-                <img src="imagenes/iconos/magic-wand.png" class="icono" alt="">
-                <div class="descripcion">Barita Magica</div>
-                <div class="menu">
-                    <div class="fondo">BARITA MAGICA</div>
-                    <div class="contenedor">
-                        <div class="titulo">
-                            BARITA MAGICA
-                        </div>
-                        <div class="gridDescripcion">
-                            <div class="item-grid">elemento 1</div>
-                            <div class="item-grid">elemento 2</div>
-                            <div class="item-grid">elemento 3</div>
-                            <div class="item-grid">elemento 4</div>
-                            <div class="item-grid">elemento 5</div>
-                            <div class="item-grid">elemento 6</div>
-                            <div class="item-grid">elemento 7</div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="imgContainer">
-                <img src="imagenes/iconos/notebook.png" class="icono" alt="">
-                <div class="descripcion">Libreta</div>
-                <div class="menu">
-                    <div class="fondo">LIBRETA</div>
-                    <div class="contenedor">
-                        <div class="titulo">
-                            LIBRETA
-                        </div>
-                        <div class="gridDescripcion">
-                            <div class="item-grid">elemento 1</div>
-                            <div class="item-grid">elemento 2</div>
-                            <div class="item-grid">elemento 3</div>
-                            <div class="item-grid">elemento 4</div>
-                            <div class="item-grid">elemento 5</div>
-                            <div class="item-grid">elemento 6</div>
-                            <div class="item-grid">elemento 7</div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="imgContainer">
-                <img src="imagenes/iconos/paint.png" class="icono" alt="">
-                <div class="descripcion">Pintura</div>
-                <div class="menu">
-                    <div class="fondo">PINTURA</div>
-                    <div class="contenedor">
-                        <div class="titulo">
-                            PINTURA
-                        </div>
-                        <div class="gridDescripcion">
-                            <div class="item-grid">elemento 1</div>
-                            <div class="item-grid">elemento 2</div>
-                            <div class="item-grid">elemento 3</div>
-                            <div class="item-grid">elemento 4</div>
-                            <div class="item-grid">elemento 5</div>
-                            <div class="item-grid">elemento 6</div>
-                            <div class="item-grid">elemento 7</div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="imgContainer">
-                <img src="imagenes/iconos/pencil.png" class="icono" alt="">
-                <div class="descripcion">Lapiz</div>
-                <div class="menu">
-                    <div class="fondo">LAPIZ</div>
-                    <div class="contenedor">
-                        <div class="titulo">
-                            LAPIZ
-                        </div>
-                        <div class="gridDescripcion">
-                            <div class="item-grid">elemento 1</div>
-                            <div class="item-grid">elemento 2</div>
-                            <div class="item-grid">elemento 3</div>
-                            <div class="item-grid">elemento 4</div>
-                            <div class="item-grid">elemento 5</div>
-                            <div class="item-grid">elemento 6</div>
-                            <div class="item-grid">elemento 7</div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="imgContainer">
-                <img src="imagenes/iconos/school-material.png" class="icono" alt="">
-                <div class="descripcion">Material Escolar</div>
-                <div class="menu">
-                    <div class="fondo">MATERIAL ESCOLAR</div>
-                    <div class="contenedor">
-                        <div class="titulo">
-                            MATERIAL ESCOLAR
-                        </div>
-                        <div class="gridDescripcion">
-                            <div class="item-grid">elemento 1</div>
-                            <div class="item-grid">elemento 2</div>
-                            <div class="item-grid">elemento 3</div>
-                            <div class="item-grid">elemento 4</div>
-                            <div class="item-grid">elemento 5</div>
-                            <div class="item-grid">elemento 6</div>
-                            <div class="item-grid">elemento 7</div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="imgContainer">
-                <img src="imagenes/iconos/brush.svg" alt="" class="icono">
-                <div class="descripcion">Brocha</div>
-                <div class="menu">
-                    <div class="fondo">BROCHA</div>
-                    <div class="contenedor">
-                        <div class="titulo">
-                            BROCHA
-                        </div>
-                        <div class="gridDescripcion">
-                            <div class="item-grid">elemento 1</div>
-                            <div class="item-grid">elemento 2</div>
-                            <div class="item-grid">elemento 3</div>
-                            <div class="item-grid">elemento 4</div>
-                            <div class="item-grid">elemento 5</div>
-                            <div class="item-grid">elemento 6</div>
-                            <div class="item-grid">elemento 7</div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="imgContainer more hidden">
-                <svg class="icono" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-                    <g>
-                        <g>
-                            <g>
-                                <path d="M256,0C114.615,0,0,114.615,0,256s114.615,256,256,256s256-114.615,256-256S397.385,0,256,0z M256,480
-				C132.288,480,32,379.712,32,256S132.288,32,256,32s224,100.288,224,224S379.712,480,256,480z" />
-                                <circle cx="256" cy="256" r="32" />
-                                <circle cx="368" cy="256" r="32" />
-                                <circle cx="144" cy="256" r="32" />
-                            </g>
-                        </g>
-                    </g>
-                </svg>
-                <div class="descripcion">Otras Opciones</div>
-                <ul class="menu_Lateral">
-                    
-                    <!--
-                    <div class="fondo">Otras Opciones</div>
-                    <div class="contenedor">
-                        <div class="titulo">
-                            Otras Opciones
-                        </div>
-                        <div class="gridDescripcion">
-
-                        </div>
-                    </div>
-                    -->
-                </ul>
-            </li>
-        </ul>
-
-    </div>
-
+    <?php include('../compartidos/menu_categorias.html'); ?>
 
     <section class="barraDirecciones">
         <div class="contenedor2">
-            <div class="caja">
-
-            </div>
+            <ul class="breadcrumbs">
+                <li class="breadcrumbs__item">
+                    <a href="" class="breadcrumbs__link">Inicio</a>
+                </li>
+                <li class="breadcrumbs__item">
+                    <a href="" class="breadcrumbs__link">Nuestros Productos</a>
+                </li>
+                <li class="breadcrumbs__item">
+                    <a href="" class="breadcrumbs__link breadcrumbs__link--active">Pinturas</a>
+                </li>
+            </ul>
         </div>
     </section>
 
-    <section>
+    <section id="contenedorPrincipal">
         <div class="contenedor3">
             <div class="caja3">
-
+                Pinturas
             </div>
             <div class="caja4">
-                <div class="barraLateral">
-
+                <div class="barraLateral hidden">
                 </div>
                 <div class="contenedorProductos">
                     <div class="caja_Productos">
@@ -532,46 +314,8 @@
         </div>
     </section>
 
-    <div class="contenido">
-        <div class="categoria">
-            Pinturas 1
-        </div>
-        <div class="categoria">
-            Pinturas 2
-        </div>
-        <div class="categoria">
-            Pinturas 3
-        </div>
-        <div class="categoria">
-            Pinturas 4
-        </div>
-        <div class="categoria">
-            Pinturas 5
-        </div>
-        <div class="categoria">
-            Pinturas 6
-        </div>
-        <div class="categoria">
-            Pinturas 7
-        </div>
-        <div class="categoria">
-            Pinturas 8
-        </div>
-        <div class="categoria">
-            Pinturas 9
-        </div>
-        <div class="categoria">
-            Pinturas 10
-        </div>
-        <div class="categoria">
-            Pinturas 11
-        </div>
-        <div class="categoria">
-            Pinturas 12
-        </div>
-    </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script type="text/javascript" src="js/productos_menu.js"></script>
+    <script type="text/javascript" src="../js/productos_menu.js"></script>
 </body>
 
 </html>
---
+++ js/productos_menu.js
@@ -0,0 +1,47 @@
+function calcWidth() {
+    /*
+    console.log("Pagina cargada productos");
+    var item_width = $('#menu li').width();
+    var morewidth = $('#menu .more').outerWidth(true);
+    console.log("menu li :"+item_width+" y more outerwidth: "+morewidth);
+
+    var item_count = ($("#menu li").length);
+    console.log("menu li count :" + item_count);
+    */
+    var elementSize = $('#menu li').outerWidth(true);
+
+    var morewidth = $('#menu .more').outerWidth(true);
+
+    var navwidth = 0;
+    $('#menu > li:not(.more)').each(function () {
+        navwidth += $(this).outerWidth(true);
+    });
+    var availablespace = $('#menu').width() - morewidth;
+    console.log("navwidth : " + navwidth + " Available space " + availablespace);
+    console.log("Llamado a calcwidth..");
+
+    if (navwidth > availablespace) {
+        console.log("Se AGREGA EL MORE");
+        var lastItem = $('#menu > li:not(.more)').last();
+        //lastItem.removeClass("imgContainer");
+        lastItem.prependTo($('#menu .more ul'));
+        calcWidth();
+    } else {
+        var firstMoreElement = $('#menu li.more li').first();
+        if (navwidth + elementSize < availablespace) {
+            //firstMoreElement.addClass("imgContainer");
+            firstMoreElement.insertBefore($('#menu .more'));
+        }
+    }
+
+    if ($('.more li').length > 0) {
+        $('.more').css('display', 'flex');
+    } else {
+        $('.more').css('display', 'none');
+    }
+}
+
+$(window).on('resize load', function () {
+
+    calcWidth();
+});
